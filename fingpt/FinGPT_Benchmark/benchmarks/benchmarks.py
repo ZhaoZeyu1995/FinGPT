4565d88d6e59f8c5f9e3d5fc567e10f9d75095f7
Zeyu Zhao <zeyuhongwu1995@gmail.com>
2024-07-21 20:51:03 +0100

    Some small changes to make the code runnable.

--- fingpt/FinGPT_Benchmark/benchmarks/benchmarks.py
+++ fingpt/FinGPT_Benchmark/benchmarks/benchmarks.py
@@ -1,3 +1,4 @@
+from fingpt.FinGPT_Benchmark.utils import parse_model_name
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel, get_peft_model, LoraConfig, TaskType  # 0.4.0
 import torch
@@ -5,7 +6,7 @@ import argparse
 
 
 from fpb import test_fpb, test_fpb_mlt
-from fiqa import test_fiqa, test_fiqa_mlt 
+from fiqa import test_fiqa, test_fiqa_mlt
 from tfns import test_tfns
 from nwgi import test_nwgi
 from headline import test_headline
@@ -17,7 +18,6 @@ from finred import test_re
 
 import sys
 sys.path.append('../')
-from utils import *
 
 
 def main(args):
@@ -25,30 +25,31 @@ def main(args):
         model_name = parse_model_name(args.base_model, args.from_remote)
     else:
         model_name = '../' + parse_model_name(args.base_model)
-        
 
     model = AutoModelForCausalLM.from_pretrained(
-        model_name, trust_remote_code=True, 
+        model_name, trust_remote_code=True,
         # load_in_8bit=True
         device_map="auto",
         # fp16=True
     )
     model.model_parallel = True
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name, trust_remote_code=True)
+
     # tokenizer.pad_token_id = tokenizer.eos_token_id
 
     tokenizer.padding_side = "left"
     if args.base_model == 'qwen':
-        tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids('<|endoftext|>')
+        tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids(
+            '<|endoftext|>')
         tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids('<|extra_0|>')
     if not tokenizer.pad_token or tokenizer.pad_token_id == tokenizer.eos_token_id:
         tokenizer.add_special_tokens({'pad_token': '[PAD]'})
         model.resize_token_embeddings(len(tokenizer))
-    
+
     print(f'pad: {tokenizer.pad_token_id}, eos: {tokenizer.eos_token_id}')
-    
+
     # peft_config = LoraConfig(
     #     task_type=TaskType.CAUSAL_LM,
     #     inference_mode=False,
@@ -61,9 +62,10 @@ def main(args):
     # model = get_peft_model(model, peft_config)
     # model.load_state_dict(torch.load(args.peft_model + '/pytorch_model.bin'))
 
-    model = PeftModel.from_pretrained(model, args.peft_model)
+    if args.peft_model:
+        model = PeftModel.from_pretrained(model, args.peft_model)
     model = model.eval()
-    
+
     with torch.no_grad():
         for data in args.dataset.split(','):
             if data == 'fpb':
@@ -90,25 +92,26 @@ def main(args):
                 test_re(args, model, tokenizer)
             else:
                 raise ValueError('undefined dataset.')
-    
+
     print('Evaluation Ends.')
-        
 
 
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", required=True, type=str)
-    parser.add_argument("--base_model", required=True, type=str, choices=['chatglm2', 'llama2', 'llama2-13b', 'llama2-13b-nr', 'baichuan', 'falcon', 'internlm', 'qwen', 'mpt', 'bloom'])
-    parser.add_argument("--peft_model", required=True, type=str)
+    parser.add_argument("--base_model", required=True, type=str, choices=[
+                        'chatglm2', 'llama2', 'llama2-13b', 'llama2-13b-nr', 'baichuan', 'falcon', 'internlm', 'qwen', 'mpt', 'bloom'])
+    parser.add_argument("--peft_model", required=False, type=str, default=None)
     parser.add_argument("--max_length", default=512, type=int)
-    parser.add_argument("--batch_size", default=4, type=int, help="The train batch size per device")
+    parser.add_argument("--batch_size", default=4, type=int,
+                        help="The train batch size per device")
     parser.add_argument("--instruct_template", default='default')
-    parser.add_argument("--from_remote", default=False, type=bool)    
+    parser.add_argument("--from_remote", default=True, type=bool)
 
     args = parser.parse_args()
-    
+
     print(args.base_model)
     print(args.peft_model)
-    
+
     main(args)
--- fingpt/FinGPT_Benchmark/data/download.py
+++ fingpt/FinGPT_Benchmark/data/download.py
@@ -6,9 +6,11 @@ DATASETS = [
     # source, destination
     (('pauri32/fiqa-2018', None), 'fiqa-2018'),
     (('FinGPT/fingpt-finred', None), 'fingpt-finred'),
-    (('zeroshot/twitter-financial-news-sentiment', None), 'twitter-financial-news-sentiment'),
+    (('zeroshot/twitter-financial-news-sentiment', None),
+     'twitter-financial-news-sentiment'),
     (('oliverwang15/news_with_gpt_instructions', None), 'news_with_gpt_instructions'),
-    (('financial_phrasebank', 'sentences_50agree'), 'financial_phrasebank-sentences_50agree'),
+    (('financial_phrasebank', 'sentences_50agree'),
+     'financial_phrasebank-sentences_50agree'),
     (('FinGPT/fingpt-fiqa_qa', None), 'fingpt-fiqa_qa'),
     (('FinGPT/fingpt-headline-cls', None), 'fingpt-headline-cls'),
     (('FinGPT/fingpt-finred', None), 'fingpt-finred'),
@@ -22,20 +24,26 @@ DATASETS = [
     (('FinGPT/fingpt-sentiment-cls', None), 'fingpt-sentiment-cls'),
 ]
 
+
 def download(no_cache: bool = False):
     """Downloads all datasets to where the FinGPT library is located."""
     data_dir = Path(__file__).parent
-    
     for src, dest in DATASETS:
         if Path(data_dir / dest).is_dir() and not no_cache:
             print(f"Dataset found at {data_dir / dest}, skipping")
             continue
         dataset = datasets.load_dataset(*src)
-        dataset.save_to_disk(data_dir / dest)
+        dataset.save_to_disk(str(data_dir / dest))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no_cache", default=False, required=False, type=str, help="Redownloads all datasets if set to True")
-    
+    parser.add_argument(
+        "--no_cache",
+        default=False,
+        required=False,
+        type=str,
+        help="Redownloads all datasets if set to True",
+    )
     args = parser.parse_args()
     download(no_cache=args.no_cache)
